af36fc19ed8f01b498d9fc8b4da63c6b99d40384
Sashank Subba <s_ubba@encs.concordia.ca>
2019-09-28 22:54:53 -0400

    [Token] Added base attributes and methods for token class.

---
+++ Token.py
@@ -0,0 +1,21 @@
+class Token:
+    """A Token in the Game"""
+
+    def __init__(self, tokenColour, tokenPosition):
+        """
+        New instance of a Token
+
+        tokenColour     Colour of the token, which could be black or red
+        tokenPosition   Position of the token on the game grid
+        """
+
+        self._tokenColour = tokenColour
+        self._tokenPosition = tokenPosition
+
+    def get_tokenColour(self):
+        """Return the colour of the token"""
+        return self._tokenColour
+
+    def get_tokenPosition(self):
+        """Return the position of the token"""
+        return self._tokenPosition
