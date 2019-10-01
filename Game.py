038f394b834f98f7ee7ac04bad3342709383b7f3
sonamtmg96 <sonamt.tmg96@gmail.com>
2019-10-01 00:04:16 -0400

    [game] changed init so that it initializes the attributes, craetes new game board and prints it.

--- Game.py
+++ Game.py
@@ -1,14 +1,15 @@
-import string
-
 class Game:
 
-	""""Represents a game session of X-Rudder"""
+	"""Represents a game session of X-Rudder"""
 	
-	def __init__(self,gameState,numberOfTotalTokens,totalTimeOfGame,gameGrid,totalPlacedTokens):
+	def __init__(self,gameState,numberOfTotalTokens,totalTimeOfGame,totalPlacedTokens):
 		self._gameState = gameState
 		self._numberOfTotalTokens = numberOfTotalTokens
-		self._gameGrid = gameGrid
+		self._gameGrid = self.createGameGrid()
 		self._totalPlacedTokens = totalPlacedTokens
+
+		print('\n','Inializiting new Game board.')
+		self.printGameGrid()
 
 	def getGameState(self):
 		"""returns the gameState"""
@@ -44,35 +45,39 @@ class Game:
 		"""returns the total placed tokens"""
 		return self._totalPlacedTokens
 		
-	def createGameGrid(self,rows,columns):
+	def createGameGrid(self):
 		"""Set gameGrid to a multidimensional 12x10 list and initiate elements to empty string."""
-		self._gameGrid = [['x' for j in range(columns)] for i in range(rows)]
+		newGrid = [['x' for j in range(12)] for i in range(10)]
+		return newGrid
 
 	def printGameGrid(self):
 		"""prints the gameGrid """
 		print("\n")
-		for i in range(0,len(self._gameGrid)):
-			row = i
-			for j in range(0,len(self._gameGrid[i])):
+		columns = ["A","B","C","D","E","F","G","H","I","J","K","L"]
+		for i in range(0,10):
+			row = i	
+			for j in range(0,12):
 				if(row == i) :
 					print(10-i,end=" ")
 					row = row - 1
 
-				print(self._gameGrid[i][j],end=" ")
+				print('{:>5}'.format(self._gameGrid[i][j]),end=" ")
 			
 			print("\n")
-
-
 			if i == 9:
-				for i in range(13):
-					print(string.ascii_uppercase[i],"", end = "")
-			
+				for i in range(len(columns)):
+					print('{:>5}'.format(columns[i]),end=" ")				
+
+		print("\n")
+
+
+
+
 
 	def checkState(self,Token):
 		"""checks the current gamestate to see if there is a win or tie, based on last token added."""
 
 
 if __name__ == "__main__":
-	game = Game("","","",[],"")
-	game.createGameGrid(10,12)
-	game.printGameGrid()
+	game = Game(False,0,"",0)
+
