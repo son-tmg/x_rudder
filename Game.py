b8a104eb033da351fab73a36bdda62fa3dd7087b
sonamtmg96 <sonamt.tmg96@gmail.com>
2019-10-03 18:55:15 -0400

    [Game] reimplemented grid

--- Game.py
+++ Game.py
@@ -1,23 +1,25 @@
+
 class Game:
 
 	"""Represents a game session of X-Rudder"""
 	
-	def __init__(self,gameState,numberOfTotalTokens,totalTimeOfGame,totalPlacedTokens):
-		self._gameState = gameState
-		self._numberOfTotalTokens = numberOfTotalTokens
+	def __init__(self,players,gameFinished,totalTimeOfGame,totalPlacedTokens,):
+		"""
+			players : list of the 2 players in the game
+			gameFinished : True if game is finished (Win or Tie). False otherwise.
+			totalTimeOfGame: total time of game
+			gameGrid: game Grid containing all tokens in the game. 
+			totalPlacedTokens : total placed tokens on board
+		"""	
+
+		self._players = players
+		self._gameFinished = gameFinished
 		self._gameGrid = self.createGameGrid()
 		self._totalPlacedTokens = totalPlacedTokens
 
-		print('\n','Inializiting new Game board.')
-		self.printGameGrid()
-
-	def getGameState(self):
-		"""returns the gameState"""
-		return self._gameState
-
-	def getNumberOfTotalTokens(self):
-		"""returns the number of total tokens"""
-		return self._numberOfTotalTokens
+	def getgameFinished(self):
+		"""returns the gameFinished"""
+		return self._gameFinished
 
 	def getGameGrid(self):
 		"""returns the gameGrid"""
@@ -27,15 +29,9 @@ class Game:
 		"""returns the total placed tokens"""
 		return self._totalPlacedTokens
 
-
-	def setGameState(self):
-		"""sets the gameState"""
-		return self._gameState
-
-	
-	def setNumberOfTotalTokens(self):
-		"""sets the number of total tokens"""
-		return self._numberOfTotalTokens
+	def setgameFinished(self):
+		"""sets the gameFinished"""
+		return self._gameFinished
 
 	def setGameGrid(self):
 		"""returns the gameGrid"""
@@ -46,38 +42,57 @@ class Game:
 		return self._totalPlacedTokens
 		
 	def createGameGrid(self):
-		"""Set gameGrid to a multidimensional 12x10 list and initiate elements to empty string."""
-		newGrid = [['x' for j in range(12)] for i in range(10)]
+		"""Set gameGrid to a multidimensional 12x10 list and initiate elements to empty string.
+			newGrid : a tuple of dictionaries. The tuples represent the rows, and each dictionary is a column 
+						Keys: A - Z , Values: Tokens
+		"""
+		newGrid = (
+					{{"A":" "},{"B":" "},{"C":" "},{"D":" "},{"E":" "},{"F":" "},{"G":" "},{"H":" "},{"I":" "},{"J":" "},{"K":" "},{"L":" "}},
+					{{"A":" "},{"B":" "},{"C":" "},{"D":" "},{"E":" "},{"F":" "},{"G":" "},{"H":" "},{"I":" "},{"J":" "},{"K":" "},{"L":" "}},
+					{{"A":" "},{"B":" "},{"C":" "},{"D":" "},{"E":" "},{"F":" "},{"G":" "},{"H":" "},{"I":" "},{"J":" "},{"K":" "},{"L":" "}},
+					{{"A":" "},{"B":" "},{"C":" "},{"D":" "},{"E":" "},{"F":" "},{"G":" "},{"H":" "},{"I":" "},{"J":" "},{"K":" "},{"L":" "}},
+					{{"A":" "},{"B":" "},{"C":" "},{"D":" "},{"E":" "},{"F":" "},{"G":" "},{"H":" "},{"I":" "},{"J":" "},{"K":" "},{"L":" "}},
+					{{"A":" "},{"B":" "},{"C":" "},{"D":" "},{"E":" "},{"F":" "},{"G":" "},{"H":" "},{"I":" "},{"J":" "},{"K":" "},{"L":" "}},
+					{{"A":" "},{"B":" "},{"C":" "},{"D":" "},{"E":" "},{"F":" "},{"G":" "},{"H":" "},{"I":" "},{"J":" "},{"K":" "},{"L":" "}},
+					{{"A":" "},{"B":" "},{"C":" "},{"D":" "},{"E":" "},{"F":" "},{"G":" "},{"H":" "},{"I":" "},{"J":" "},{"K":" "},{"L":" "}},
+					{{"A":" "},{"B":" "},{"C":" "},{"D":" "},{"E":" "},{"F":" "},{"G":" "},{"H":" "},{"I":" "},{"J":" "},{"K":" "},{"L":" "}},
+					{{"A":" "},{"B":" "},{"C":" "},{"D":" "},{"E":" "},{"F":" "},{"G":" "},{"H":" "},{"I":" "},{"J":" "},{"K":" "},{"L":" "}}
+				)
+		print(newGrid[0])
 		return newGrid
 
 	def printGameGrid(self):
-		"""prints the gameGrid """
-		print("\n")
-		columns = ["A","B","C","D","E","F","G","H","I","J","K","L"]
-		for i in range(0,10):
-			row = i	
-			for j in range(0,12):
-				if(row == i) :
-					print(10-i,end=" ")
-					row = row - 1
 
-				print('{:>5}'.format(self._gameGrid[i][j]),end=" ")
+		"""prints the gameGrid as a 10 x 12 matrix"""
+		print("\n")
+		
+		for i in range(self._gameGrid):
 			
+
+
+
+
+		# columns = ["A","B","C","D","E","F","G","H","I","J","K","L"]
+		# for i in range(0,10):
+		# 	row = i	
+		# 	for j in range(0,12):
+		# 		if(row == i) :
+		# 			print(10-i,end=" ")
+		# 			row = row - 1
+
+		# 		print('{:>5}'.format(self._gameGrid[i][j]),end=" ")
+			
+		# 	print("\n")
+		# 	if i == 9:
+		# 		for i in range(len(columns)):
+		# 			print('{:>5}'.format(columns[i]),end=" ")				
+
 			print("\n")
-			if i == 9:
-				for i in range(len(columns)):
-					print('{:>5}'.format(columns[i]),end=" ")				
-
-		print("\n")
-
-
-
 
 
 	def checkState(self,Token):
-		"""checks the current gamestate to see if there is a win or tie, based on last token added."""
+		"""checks the current gameFinished to see if there is a win or tie, based on last token added."""
 
 
 if __name__ == "__main__":
-	game = Game(False,0,"",0)
-
+	g1 = Game("","","","")
