8396c34a59c2cd104045a9adbc1032ae2f7c79ae
Sashank Subba <s_ubba@encs.concordia.ca>
2019-10-05 18:56:57 -0400

    [Main_Class] Added features to main class and starting game options.

--- gameStart.py
+++ gameStart.py
@@ -1,19 +1,31 @@
 import Game,Player,Token
 
 if __name__ == "__main__":
-    print("Welcome to X-rudder.")
+    startState = False
+    Players = []
 
-    gameMode = input("Which mode would you like to play? : 1- Manual 2-CPU 3-A")
+    print("--------------------------------------------------")
+    print("Welcome to the 2-player game called X-Rudder.\n")
 
-    if gameMode == "1":
-        players = []
+    while startState == False:
+        gameMode = input("Which mode would you like to play? (1-Player vs Player, 2-Player vs AI): ")
 
-        player1 = Player("Black")
-        player1Name = input("Player1 enter your name: ")
+        if gameMode == "1":
+            startState = True
 
-        player2 = Player("Red")
-        player2Name = input("Player2 enter your name: ")
+            Player1Name = input("Welcome Player 1, please enter your name: ")
+            Player1 = Player(Player1Name, "u\u2588")
 
-        print("Starting a new game session.")
-        newGame = Game()
-        players.append(player1,player2)
+            Player2Name = input("Welcome Player 2, please enter your name: ")
+            Player2 = Player(Player2Name, "u\u2591")
+
+            print("Starting a new game session.")
+            newGame = Game()
+            players.append(player1,player2)
+
+        elif gameMode == "2":
+            startState = True
+            print("\nNOT IMPLEMENTED YET. GOODBYE.")
+
+        else:
+            print("\nInvalid Option. Please try again.\n")
