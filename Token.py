cc7eb19b93d23a27b4c46290244ec2c772526a48
sonamtmg96 <sonamt.tmg96@gmail.com>
2019-10-05 21:01:28 -0400

    [gameGrid] Reimplemented game grid as a list of lists for easier indexing.Also added updateGrid method WIP

--- Token.py
+++ Token.py
@@ -1,12 +1,17 @@
 class Token:
     """A Token in the Game"""
 
+    #class variables representing the unicode for black and white colored tokens
+    white = 'u\u2588'
+    black = 'u\u2591'
+
+
     def __init__(self, tokenColour, tokenPosition):
         """
         New instance of a Token
 
-        tokenColour     Colour of the token, which could be black or red
-        tokenPosition   Position of the token on the game grid
+        tokenColour     String representing the Colour of the token, which could be white or black in unicode.
+        tokenPosition   list representing Position of the token on the game grid. Always in pairs [i,j], where i is row and j is column
         """
 
         self._tokenColour = tokenColour
@@ -19,3 +24,11 @@ class Token:
     def get_tokenPosition(self):
         """Return the position of the token"""
         return self._tokenPosition
+
+    def set_tokenColour(self,tokenColour):
+        """Set the colour of the token"""
+        self._tokenColour = tokenColour
+
+    def set_tokenPosition(self,tokenPosition):
+        """Set the position of the token"""
+        self._tokenPosition = [tokenPosition[0],tokenPosition[1]]
