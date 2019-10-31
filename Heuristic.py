7885acda454d8f231333966b794ef64cea644020
Sashank Subba <s_ubba@encs.concordia.ca>
2019-10-31 18:46:23 -0400

    [Heuristic] Finished searchScore method for heuristic features 1-4.

--- Heuristic.py
+++ Heuristic.py
@@ -3,38 +3,62 @@ import Game, Player, Token
 class Heuristic:
     """The heuristic function used for the AI"""
 
-    Score = 0
-    maxDepth = 0
-
     def searchList(self, game, searchPosition, maxDepth = 1):
         """
-        Method to get the list of positions to check
+        Method to get the list of positions to check, returning the positions which have the highest evaluation
 
-        searchPosition      Position coordinate to search and dictate the rest of potential moves
-        maxDepth            maximum perimeter depth the function will look into to find the score
+        game                The game that is being played on
+        searchPosition      The current grid position being evaluated, with its specified perimeter positions
+        maxDepth            The maximum perimeter depth the function will look into to find the score
         """
-        searchList = []
         y = searchPosition[0]
         x = searchPosition[1]
-        maxScore = self.searchScore(game, searchPosition)
+        maxList = []
 
-        if (0 <= y-maxDepth <= 9 and 0 <= x-maxDepth <= 11) and (0 <= y+maxDepth <= 9 and 0 <= x+maxDepth <= 11):
-            for i in range(y-maxDepth, y+(maxDepth+1)):
-                for j in range(x-maxDepth, x+(maxDepth+1)):
-                    searchList.append([i,j])
-
-            return self.searchScore(searchList)
-
+        if game.getGameGrid()[y][x] is None:
+            maxScore = self.searchScore(game, searchPosition)
+            maxList.append(searchPosition)
         else:
-            print("need to check and change maxDepth. WIP")
+            maxScore = 0
+
+        #if (0 <= y-maxDepth <= 9 and 0 <= x-maxDepth <= 11) and (0 <= y+maxDepth <= 9 and 0 <= x+maxDepth <= 11):
+        for i in range(y-maxDepth, y+(maxDepth+1)):
+            for j in range(x-maxDepth, x+(maxDepth+1)):
+                if (0 <= i <= 9 and 0 <= j <= 11):
+                    if self.searchScore(game, [i, j]) > maxScore:
+                        maxScore = self.searchScore(game, [i, j])
+                        for element in maxList:
+                            maxList.pop()
+                        maxList.append([i,j])
+                    elif self.searchScore(game, [i, j]) == maxScore:
+                        maxList.append([i, j])
+
+        return maxList
 
     def searchScore(self, game, searchPosition):
         """
         Method to get the score of each individual search positions
 
-        game
-        searchPosition
+        game                The game that is being played on
+        searchPosition      The current grid position being evaluated
         """
+        y = searchPosition[0]
+        x = searchPosition[1]
         score1 = 0.2 * (15 - game.getPlayers()[0].get_nbTokens())
-        score2 = 0.1 * (15 - game.getPlayers()[1].get_nbTokens())
-        score3 = 0.2
+        score2 = -0.1 * (15 - game.getPlayers()[1].get_nbTokens())
+        score3 = 0
+        score4 = 0
+
+        for i in range(y - 1, y + 2):
+            for j in range(x - 1, x + 2):
+                if (0 <= i <= 9 and 0 <= j <= 11) and (game.getGameGrid()[i][j] is not None):
+                    if game.getGameGrid()[i][j].get_tokenColour() == game.getPlayers()[0].get_playerColour():
+                        score3 += 1
+                    else:
+                        score4 += 1
+
+        score3 *= 0.2
+        score4 *= -0.1
+
+        #Addition of score5 from heuristic feature 5 * weight of function 5
+        return (score1 + score2 + score3 + score4)
