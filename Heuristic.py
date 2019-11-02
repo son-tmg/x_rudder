641ded224a5ecc52fde3e5b82201b298dbc503f8
sonamtmg96 <sonamt.tmg96@gmail.com>
2019-11-02 17:35:56 -0400

    [Heuristic] added score5 and modified getNumberOfWinningConfigurations for the case that a winning config has a half crossed winning configuration

--- Heuristic.py
+++ Heuristic.py
@@ -47,7 +47,8 @@ class Heuristic:
         score1 = 0.2 * (15 - game.getPlayers()[0].get_nbTokens())
         score2 = -0.1 * (15 - game.getPlayers()[1].get_nbTokens())
         score3 = 0
-        score4 = self.getNumberOfWinningConfigurations(self,searchPosition,game)
+        score4 = 0
+        score5 = 0.4*self.getNumberOfWinningConfigurations(self,searchPosition,game)
 
         for i in range(y - 1, y + 2):
             for j in range(x - 1, x + 2):
@@ -84,70 +85,71 @@ class Heuristic:
 		#Center
         if ( 1<=i<=8 and 1<=j<=10):
 			if (
-			    gameGrid[i+1][j+1] == None or gameGrid[i+1][j+1].get_tokenColour() == "\u2588   " and
-				gameGrid[i+1][j-1] == None or gameGrid[i+1][j-1].get_tokenColour() == "\u2588   " and
-				gameGrid[i-1][j+1] == None or gameGrid[i-1][j+1].get_tokenColour() ==  "\u2588   " and 
-				gameGrid[i-1][j-1] == None or gameGrid[i-1][j-1].get_tokenColour() == "\u2588   "
+			    gameGrid[i+1][j+1] == None or gameGrid[i+1][j+1].get_tokenColour() == game.getPlayers()[0].get_playerColour() and
+				gameGrid[i+1][j-1] == None or gameGrid[i+1][j-1].get_tokenColour() == game.getPlayers()[0].get_playerColour() and
+				gameGrid[i-1][j+1] == None or gameGrid[i-1][j+1].get_tokenColour() ==  game.getPlayers()[0].get_playerColour() and 
+				gameGrid[i-1][j-1] == None or gameGrid[i-1][j-1].get_tokenColour() == game.getPlayers()[0].get_playerColour()
 			):
 				if (
-					gameGrid[i][j-1] == None or gameGrid[i][j-1].get_tokenColour() == "\u2588   "  or 
-					gameGrid[i][j+1] == None or gameGrid[i][j+1].get_tokenColour() == "\u2588   "
+					gameGrid[i][j-1] == None or gameGrid[i][j-1].get_tokenColour() == game.getPlayers()[0].get_playerColour()  and
+					gameGrid[i][j+1] == None or gameGrid[i][j+1].get_tokenColour() == game.getPlayers()[0].get_playerColour() 
+
 					):
 						numberOfWinningConfigurations += 1
 
         #top left						
-        if ( 0<=i<=7 and 0<=j<=9 ):		
+        if ( 0<=i<=7 and 0<=j<=9 ):
 			if (
-				gameGrid[i][j+2] == None or gameGrid[i][j+2].get_tokenColour() == "\u2588   " and
-				gameGrid[i+1][j+1] == None or gameGrid[i+1][j+1].get_tokenColour() == "\u2588   " and
-				gameGrid[i+2][j] == None or gameGrid[i+2][j].get_tokenColour() == "\u2588   " and 
-				gameGrid[i+2][j+2] == None or gameGrid[i+2][j+2].get_tokenColour() == "\u2588   "
+				gameGrid[i][j+2] == None or gameGrid[i][j+2].get_tokenColour() == game.getPlayers()[0].get_playerColour() and
+				gameGrid[i+1][j+1] == None or gameGrid[i+1][j+1].get_tokenColour() == game.getPlayers()[0].get_playerColour() and
+				gameGrid[i+2][j] == None or gameGrid[i+2][j].get_tokenColour() == game.getPlayers()[0].get_playerColour() and 
+				gameGrid[i+2][j+2] == None or gameGrid[i+2][j+2].get_tokenColour() == game.getPlayers()[0].get_playerColour()
 			):
 				if (
-					gameGrid[i+1][j] == None or gameGrid[i+1][j].get_tokenColour() == "\u2588   " or
-					gameGrid[i+1][j+2] == None or gameGrid[i+1][j+2].get_tokenColour() == "\u2588   "
+					gameGrid[i+1][j] == None or gameGrid[i+1][j].get_tokenColour() == game.getPlayers()[0].get_playerColour() and
+					gameGrid[i+1][j+2] == None or gameGrid[i+1][j+2].get_tokenColour() == game.getPlayers()[0].get_playerColour()
 					):
 					    numberOfWinningConfigurations += 1
 
 		#top right
         if ( 0<=i<=7 and 2<=j<=11 ):	
             if (
-                gameGrid[i][j-2] == None or gameGrid[i][j-2].get_tokenColour() == "\u2588   " and
-                gameGrid[i+1][j-1] == None or gameGrid[i+1][j-1].get_tokenColour() == "\u2588   " and
-                gameGrid[i+2][j-2] == None or gameGrid[i+2][j-2].get_tokenColour() == "\u2588   " and 
-                gameGrid[i+2][j] == None or gameGrid[i+2][j].get_tokenColour() == "\u2588   "
+                gameGrid[i][j-2] == None or gameGrid[i][j-2].get_tokenColour() == game.getPlayers()[0].get_playerColour() and
+                gameGrid[i+1][j-1] == None or gameGrid[i+1][j-1].get_tokenColour() == game.getPlayers()[0].get_playerColour() and
+                gameGrid[i+2][j-2] == None or gameGrid[i+2][j-2].get_tokenColour() == game.getPlayers()[0].get_playerColour() and 
+                gameGrid[i+2][j] == None or gameGrid[i+2][j].get_tokenColour() == game.getPlayers()[0].get_playerColour()
             ):
                 if (
-                    gameGrid[i+1][j] == None or gameGrid[i+1][j].get_tokenColour() == "\u2588   " or
-                    gameGrid[i+1][j-2] == None or gameGrid[i+1][j-2].get_tokenColour() == "\u2588   "
+                    gameGrid[i+1][j] == None or gameGrid[i+1][j].get_tokenColour() == game.getPlayers()[0].get_playerColour() and
+                    gameGrid[i+1][j-2] == None or gameGrid[i+1][j-2].get_tokenColour() == game.getPlayers()[0].get_playerColour()
                     ):
                         numberOfWinningConfigurations += 1
 
         #bottom left
 		if (2<=i<=9 and 0<=j<=9):
 			if (
-				gameGrid[i-2][j] == None or gameGrid[i-2][j].get_tokenColour() == "\u2588   " and
-				gameGrid[i][j+2] == None or gameGrid[i][j+2].get_tokenColour() == "\u2588   " and
-				gameGrid[i-1][j+1] == None or gameGrid[i-1][j+1].get_tokenColour() == "\u2588   " and 
-				gameGrid[i-2][j+2] == None or gameGrid[i-2][j+2].get_tokenColour() == "\u2588   "
+				gameGrid[i-2][j] == None or gameGrid[i-2][j].get_tokenColour() == game.getPlayers()[0].get_playerColour() and
+				gameGrid[i][j+2] == None or gameGrid[i][j+2].get_tokenColour() == game.getPlayers()[0].get_playerColour() and
+				gameGrid[i-1][j+1] == None or gameGrid[i-1][j+1].get_tokenColour() == game.getPlayers()[0].get_playerColour() and 
+				gameGrid[i-2][j+2] == None or gameGrid[i-2][j+2].get_tokenColour() == game.getPlayers()[0].get_playerColour()
 			):
 				if (
-					gameGrid[i-1][j] == None or gameGrid[i-1][j].get_tokenColour() == "\u2588   " or
-					gameGrid[i-1][j+2] == None or gameGrid[i-1][j+2].get_tokenColour() == "\u2588   "
+					gameGrid[i-1][j] == None or gameGrid[i-1][j].get_tokenColour() == game.getPlayers()[0].get_playerColour() and
+					gameGrid[i-1][j+2] == None or gameGrid[i-1][j+2].get_tokenColour() == game.getPlayers()[0].get_playerColour()
 					):
 						numberOfWinningConfigurations += 1
 
 		#bottom right			
 		if (2<=i<=9 and 2<=j<=11):		
 			if (
-				gameGrid[i-2][j] == None or gameGrid[i-2][j].get_tokenColour() == "\u2588   " and
-				gameGrid[i-1][j-1] == None or gameGrid[i-1][j-1].get_tokenColour() == "\u2588   " and
-				gameGrid[i][j-2] == None or gameGrid[i][j-2].get_tokenColour() == "\u2588   " and 
-				gameGrid[i-2][j-2] == None or gameGrid[i-2][j-2].get_tokenColour() == "\u2588   "
+				gameGrid[i-2][j] == None or gameGrid[i-2][j].get_tokenColour() == game.getPlayers()[0].get_playerColour() and
+				gameGrid[i-1][j-1] == None or gameGrid[i-1][j-1].get_tokenColour() == game.getPlayers()[0].get_playerColour() and
+				gameGrid[i][j-2] == None or gameGrid[i][j-2].get_tokenColour() == game.getPlayers()[0].get_playerColour() and 
+				gameGrid[i-2][j-2] == None or gameGrid[i-2][j-2].get_tokenColour() == game.getPlayers()[0].get_playerColour()
 			):
 				if (
-					gameGrid[i-1][j] == None or gameGrid[i-1][j].get_tokenColour() == "\u2588   " or
-					gameGrid[i-1][j-2] == None or gameGrid[i-1][j-2].get_tokenColour() == "\u2588   "
+					gameGrid[i-1][j] == None or gameGrid[i-1][j].get_tokenColour() == game.getPlayers()[0].get_playerColour() and
+					gameGrid[i-1][j-2] == None or gameGrid[i-1][j-2].get_tokenColour() == game.getPlayers()[0].get_playerColour()
 					):
 						numberOfWinningConfigurations += 1
 
